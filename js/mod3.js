cce8e1f6ba1282010ef19d9b71a58f02259140e1
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-10-26 21:44:04 +0200

    Добавляет примеры

--- js/mod3.js
+++ js/mod3.js
@@ -1,4 +1,32 @@
-const foo = function (x, y) {
-  return x + y;
+// const foo = function (x, y) {
+//   return x + y;
+// };
+// console.log(foo(1, 2));
+
+const countTotalSalary = function (employees) {
+  "use strict";
+  // Write code under this line
+  const values = Object.values(employees);
+  let total = 0;
+  for (const value of values) {
+    total += value;
+  }
+  return total;
 };
-console.log(foo(1, 2));
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
